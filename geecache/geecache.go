69100954caab362a672866ba12ea16f79a45a337
Thinkingward <1339180450@qq.com>
2025-03-18 19:31:36 +0800

    完成了对lru的封装也就是cache.go，ByteView只读结构用来表示缓存值，完成group负责与外部交互，实现了单机并发缓存

---
+++ geecache/geecache.go
@@ -0,0 +1,86 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// 数据源很多，需要区分不同的数据源，通过GetterFunc类型实现Getter接口
+// 这样就可以将任意函数类型转换为Getter类型，由用户自己实现
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// 创建一个新的Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// 根据name获取Group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+// 根据key获取value
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
--- geecache/go.mod
+++ geecache/go.mod
@@ -1,3 +1,3 @@
-module lru
+module geecache
 
 go 1.23.6
