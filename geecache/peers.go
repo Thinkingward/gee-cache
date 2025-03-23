8683a02a334047cb9063f2fd5d46135d258d868c
Thinkingward <1339180450@qq.com>
2025-03-23 14:12:52 +0800

    将通信的中间载体换成protobuf，优化节点间通信功能

--- geecache/peers.go
+++ geecache/peers.go
@@ -1,5 +1,7 @@
 package geecache
 
+import pb "geecache/geecachepb"
+
 //根据传入的key选择相应节点PeerGetter
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
@@ -7,5 +9,5 @@ type PeerPicker interface {
 
 //从对应的group查找缓存值
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
